d767d2ad101ec7f19ec420cbf125caa7f31bbf01
Abdelrahman moustafa faraj <121641142+Abdelrahman22322@users.noreply.github.com>
2023-12-03 04:30:04 +0200

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.file_name = new System.Windows.Forms.TextBox();
+            this.file_content = new System.Windows.Forms.RichTextBox();
+            this.opration = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // file_name
+            // 
+            this.file_name.Location = new System.Drawing.Point(334, 108);
+            this.file_name.Name = "file_name";
+            this.file_name.Size = new System.Drawing.Size(161, 24);
+            this.file_name.TabIndex = 0;
+            // 
+            // file_content
+            // 
+            this.file_content.Location = new System.Drawing.Point(260, 208);
+            this.file_content.Name = "file_content";
+            this.file_content.Size = new System.Drawing.Size(310, 197);
+            this.file_content.TabIndex = 1;
+            this.file_content.Text = "";
+            // 
+            // opration
+            // 
+            this.opration.Location = new System.Drawing.Point(370, 165);
+            this.opration.Name = "opration";
+            this.opration.Size = new System.Drawing.Size(75, 23);
+            this.opration.TabIndex = 2;
+            this.opration.Text = "opration";
+            this.opration.UseVisualStyleBackColor = true;
+            this.opration.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.opration_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "file_name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.opration);
+            this.Controls.Add(this.file_content);
+            this.Controls.Add(this.file_name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox file_name;
+        private System.Windows.Forms.RichTextBox file_content;
+        private System.Windows.Forms.Button opration;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.AccessControl;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+
+        private void opration_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case 'c':
+                case 'C':
+                    if (File.Exists(file_name.Text))
+                    {
+                        MessageBox.Show("This file has already been created");
+                    }
+                    else
+                    {
+                        FileStream Fs = new FileStream(file_name.Text, FileMode.Create, FileAccess.ReadWrite);
+                        
+                        Fs.Close();
+                    }
+
+                    break;
+                case 'd':
+                case 'D':
+                    if (File.Exists(file_name.Text))
+                    {
+                        File.Delete(file_name.Text);
+                    }
+                    else
+                        MessageBox.Show("This File does Not exist");
+
+                    break;
+
+                case 'o':
+                case 'O':
+                    if (File.Exists(file_name.Text))
+                    {
+                        FileStream fs = new FileStream(file_name.Text, FileMode.Open, FileAccess.Read);
+
+                        StreamReader str = new StreamReader(fs);
+                            
+                                str.BaseStream.Seek(0, SeekOrigin.Begin);
+                                file_content.Text = str.ReadToEnd();
+                             
+                                str.Close();
+                                fs.Close();
+                            
+
+                            //byte[] Bytes = new byte[fss.Length];
+                            //int NumberBytesToRead = (int)fss.Length;
+                            //int ByteRead = 0;
+
+                            //while (NumberBytesToRead > 0)
+                            //{
+                            //    int n = fss.Read(Bytes, ByteRead, NumberBytesToRead);
+
+                            //    if (n == 0)
+                            //        break;
+                            //    NumberBytesToRead -= n;
+                            //    ByteRead += n;
+
+
+                            //}
+
+
+                            //foreach (var VARIABLE in Bytes)
+                            //{
+                            //    Console.Write((char)VARIABLE);
+                            //}
+
+                           
+
+                        
+                    }
+                    else
+                        MessageBox.Show("This file does not exist");
+
+                    break;
+                case 's':
+                case 'S':
+                    
+                    FileStream fss = new FileStream(file_name.Text, FileMode.Open, FileAccess.Write);
+
+                    
+                    StreamWriter strW = new StreamWriter(fss);
+                        
+                            strW.Write(file_content.Text);
+                            strW.Flush();
+
+                       strW.Close();
+                    fss.Close();
+                    
+
+                    break;
+
+                case '?':
+                case '؟':
+
+                    MessageBox.Show("c =create \nd =delete \no =open \ns =save");
+
+                    break;
+
+
+
+            }
+        }
+    }
+}
